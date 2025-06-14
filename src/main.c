b98ff2fe893d483f74931ccb207f32489f0fc01d
lHuanYul <joe2345698@gmail.com>
2025-06-14 23:45:44 +0800

    upd

--- src/main.c
+++ src/main.c
@@ -27,6 +27,7 @@ void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(esp_netif_init());
+    
     wifi_init_sta();
     wifi_connect_sta();
 
